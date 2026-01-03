00e5502208cc9ea4619fa7db8c246cdf6c7d5fd3
domnul-zeu <41903804+edholofy@users.noreply.github.com>
2026-01-03 18:30:20 +0100

    Fix formula: Include Language::Python::Virtualenv, update to 0.4.4

--- Formula/some1.rb
+++ Formula/some1.rb
@@ -1,18 +1,19 @@
 class Some1 < Formula
+  include Language::Python::Virtualenv
+
   desc "Autonomous AI Employees That Never Sleep"
   homepage "https://github.com/edholofy/some1-cli"
-  url "https://files.pythonhosted.org/packages/source/s/some1/some1-0.4.2.tar.gz"
-  sha256 "37d1b81f6bee4f61f3b0c938f56e6a14429711472ef7c3e43655cab76689df12"
+  url "https://files.pythonhosted.org/packages/source/s/some1/some1-0.4.4.tar.gz"
+  sha256 "80cf303119b4bd63223c2fbb6ff73179dc29820657f37d11453a019654727671"
   license "MIT"
 
   depends_on "python@3.11"
 
   def install
-    venv = virtualenv_create(libexec, "python3.11")
-    venv.pip_install_and_link buildpath
+    virtualenv_install_with_resources
   end
 
   test do
-    system "false"
+    assert_match "SOME-1", shell_output("#{bin}/some1 --version 2>&1", 2)
   end
 end
